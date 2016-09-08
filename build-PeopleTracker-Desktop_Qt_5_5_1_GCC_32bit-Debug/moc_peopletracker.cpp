f45a0bf33a710fe6e1d02037d041b245ae9a6cf5
dkreider09@gmail.com <dkreider09@gmail.com>
2016-09-08 19:41:06 -0300

    updated build

--- build-PeopleTracker-Desktop_Qt_5_5_1_GCC_32bit-Debug/moc_peopletracker.cpp
+++ build-PeopleTracker-Desktop_Qt_5_5_1_GCC_32bit-Debug/moc_peopletracker.cpp
@@ -19,8 +19,8 @@
 
 QT_BEGIN_MOC_NAMESPACE
 struct qt_meta_stringdata_peopleTracker_t {
-    QByteArrayData data[17];
-    char stringdata0[372];
+    QByteArrayData data[19];
+    char stringdata0[427];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -45,7 +45,9 @@ QT_MOC_LITERAL(12, 242, 25), // "on_actionDelete_triggered"
 QT_MOC_LITERAL(13, 268, 28), // "on_actionFind_name_triggered"
 QT_MOC_LITERAL(14, 297, 23), // "on_saveButton_2_clicked"
 QT_MOC_LITERAL(15, 321, 23), // "on_updateButton_clicked"
-QT_MOC_LITERAL(16, 345, 26) // "on_searchBox_returnPressed"
+QT_MOC_LITERAL(16, 345, 26), // "on_searchBox_returnPressed"
+QT_MOC_LITERAL(17, 372, 26), // "on_actionRestore_triggered"
+QT_MOC_LITERAL(18, 399, 27) // "on_actionBackup_2_triggered"
 
     },
     "peopleTracker\0on_clearButton_clicked\0"
@@ -59,7 +61,9 @@ QT_MOC_LITERAL(16, 345, 26) // "on_searchBox_returnPressed"
     "on_actionDelete_triggered\0"
     "on_actionFind_name_triggered\0"
     "on_saveButton_2_clicked\0on_updateButton_clicked\0"
-    "on_searchBox_returnPressed"
+    "on_searchBox_returnPressed\0"
+    "on_actionRestore_triggered\0"
+    "on_actionBackup_2_triggered"
 };
 #undef QT_MOC_LITERAL
 
@@ -69,7 +73,7 @@ static const uint qt_meta_data_peopleTracker[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-      15,   14, // methods
+      17,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -77,21 +81,23 @@ static const uint qt_meta_data_peopleTracker[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   89,    2, 0x08 /* Private */,
-       3,    0,   90,    2, 0x08 /* Private */,
-       4,    0,   91,    2, 0x08 /* Private */,
-       5,    0,   92,    2, 0x08 /* Private */,
-       6,    0,   93,    2, 0x08 /* Private */,
-       7,    0,   94,    2, 0x08 /* Private */,
-       8,    0,   95,    2, 0x08 /* Private */,
-       8,    1,   96,    2, 0x08 /* Private */,
-      10,    0,   99,    2, 0x08 /* Private */,
-      11,    0,  100,    2, 0x08 /* Private */,
-      12,    0,  101,    2, 0x08 /* Private */,
-      13,    0,  102,    2, 0x08 /* Private */,
-      14,    0,  103,    2, 0x08 /* Private */,
-      15,    0,  104,    2, 0x08 /* Private */,
-      16,    0,  105,    2, 0x08 /* Private */,
+       1,    0,   99,    2, 0x08 /* Private */,
+       3,    0,  100,    2, 0x08 /* Private */,
+       4,    0,  101,    2, 0x08 /* Private */,
+       5,    0,  102,    2, 0x08 /* Private */,
+       6,    0,  103,    2, 0x08 /* Private */,
+       7,    0,  104,    2, 0x08 /* Private */,
+       8,    0,  105,    2, 0x08 /* Private */,
+       8,    1,  106,    2, 0x08 /* Private */,
+      10,    0,  109,    2, 0x08 /* Private */,
+      11,    0,  110,    2, 0x08 /* Private */,
+      12,    0,  111,    2, 0x08 /* Private */,
+      13,    0,  112,    2, 0x08 /* Private */,
+      14,    0,  113,    2, 0x08 /* Private */,
+      15,    0,  114,    2, 0x08 /* Private */,
+      16,    0,  115,    2, 0x08 /* Private */,
+      17,    0,  116,    2, 0x08 /* Private */,
+      18,    0,  117,    2, 0x08 /* Private */,
 
  // slots: parameters
     QMetaType::Void,
@@ -102,6 +108,8 @@ static const uint qt_meta_data_peopleTracker[] = {
     QMetaType::Void,
     QMetaType::QString,
     QMetaType::Void, QMetaType::QModelIndex,    9,
+    QMetaType::Void,
+    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
@@ -124,7 +132,6 @@ void peopleTracker::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _i
         case 2: _t->on_deleteButton_clicked(); break;
         case 3: _t->on_actionExit_triggered(); break;
         case 4: _t->on_actionAbout_triggered(); break;
-
         case 6: { QString _r = _t->on_listView_clicked();
             if (_a[0]) *reinterpret_cast< QString*>(_a[0]) = _r; }  break;
         case 7: _t->on_listView_clicked((*reinterpret_cast< const QModelIndex(*)>(_a[1]))); break;
@@ -132,9 +139,10 @@ void peopleTracker::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _i
         case 9: _t->on_actionClose_triggered(); break;
         case 10: _t->on_actionDelete_triggered(); break;
         case 11: _t->on_actionFind_name_triggered(); break;
-
         case 13: _t->on_updateButton_clicked(); break;
         case 14: _t->on_searchBox_returnPressed(); break;
+        case 15: _t->on_actionRestore_triggered(); break;
+        case 16: _t->on_actionBackup_2_triggered(); break;
         default: ;
         }
     }
@@ -165,13 +173,13 @@ int peopleTracker::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 15)
+        if (_id < 17)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 15;
+        _id -= 17;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 15)
+        if (_id < 17)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 15;
+        _id -= 17;
     }
     return _id;
 }
